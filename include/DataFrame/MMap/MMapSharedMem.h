e8a39005dab7b30ff459120959d25622fc39eab5
Jimmy M. Gong <jimmy.m.gong@gmail.com>
2020-02-09 17:29:03 +0800

    GENERATE_EXPORT_HEADER, static, shared

--- include/DataFrame/MMap/MMapSharedMem.h
+++ include/DataFrame/MMap/MMapSharedMem.h
@@ -37,8 +37,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 namespace hmdf
 {
-
-class   MMapSharedMem : public MMapBase  {
+class LIBRARY_API MMapSharedMem : public MMapBase {
 
 public:
 
